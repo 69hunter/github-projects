9e823eaa1b7765e74356c528c8eac02aba7d3201
khunlingtan <kltan69@gmail.com>
2019-03-09 16:22:26 +0800

    added link to home at navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <b-navbar type="dark" variant="primary" :sticky="true">
-      <b-navbar-brand>Github Projects</b-navbar-brand>
+      <b-navbar-brand :to="{ name: 'home'}">
+        Github Projects
+      </b-navbar-brand>
     </b-navbar>
     <div class="container my-3">
       <router-view></router-view>
