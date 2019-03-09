8ec445eb8eae332637027c9e951483c8a058d9bd
khunlingtan <kltan69@gmail.com>
2019-03-09 15:56:45 +0800

    created project page

--- src/api/api-github.js
+++ src/api/api-github.js
@@ -10,4 +10,19 @@ export default {
       })
       .then(response => response.json());
   },
+  getProjectDetails(userid, projectid) {
+    const url = `https://api.github.com/repos/${userid}/${projectid}/readme`;
+    return fetch(url, {
+      headers: {
+        Accept: 'application/vnd.github.v3.html'
+      }
+    })
+      .then(response => {
+        if (response.status !== 200) {
+          throw Error('Not found')
+        }
+        return response;
+      })
+      .then(response => response.text());
+  },
 }
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,14 +1,72 @@
 <template>
   <div>
-    Project
+    <b-breadcrumb :items="items"/>
+    <b-alert
+      :show="showAlert"
+      variant="danger"
+      class="d-flex justify-content-between align-items-center"
+    >
+      <div>Project details not found</div>
+      <b-button variant="outline-danger" @click="$router.go(-1)">Back</b-button>
+    </b-alert>
+    <div v-if="!showAlert">
+      <h1 class="h2">{{this.$route.params.projectid}}</h1>
+      <div v-html="content"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import ApiGithub from "../api/api-github";
+
 export default {
   name: "Project",
+  data: function() {
+    return {
+      items: [
+        {
+          text: "Home",
+          to: { name: "home" }
+        },
+        {
+          text: "User",
+          to: { name: "user", userid: this.$route.params.userid }
+        },
+        {
+          text: "Project",
+          active: true
+        }
+      ],
+      showAlert: true,
+      content: ""
+    };
+  },
+  created() {
+    this.getProjectDetails(
+      this.$route.params.userid,
+      this.$route.params.projectid
+    );
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.getProjectDetails(to.params.userid, to.params.projectid);
+    next();
+  },
+  methods: {
+    getProjectDetails(userid, projectid) {
+      ApiGithub.getProjectDetails(userid, projectid)
+        .then(response => {
+          console.log(response);
+          return response;
+        })
+        .then(response => {
+          this.showAlert = false;
+          this.content = response;
+        })
+        .catch(err => {
+          this.showAlert = true;
+          this.content = "";
+        });
+    }
+  }
 };
 </script>
-
-<style scoped>
-</style>
